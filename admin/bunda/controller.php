39876aeca61390903e8062f5773a11c20d83f3a6
azwarbahar07 <azwarbahar07@gmail.com>
2021-07-02 23:00:06 +0800

    create crud bunda

---
+++ admin/bunda/controller.php
@@ -0,0 +1,131 @@
+<?php
+function plugins() { ?>
+	<link rel="stylesheet" href="/apmodasi/assets/plugins/bootstrap-more/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/apmodasi/assets/dist/css2/components.css">
+	<script src="/apmodasi/assets/dist/jquery.min.js"></script>
+	<script src="/apmodasi/assets/dist/sweetalert/sweetalert.min.js"></script>
+<?php }
+require('../../koneksi.php');
+
+
+// SUBMIT bunda
+if (isset($_POST['submit_bunda'])) {
+	$nama_bunda = $_POST['nama_bunda'];
+	$kontak_bunda = $_POST['kontak_bunda'];
+	$alamat_bunda = $_POST['alamat_bunda'];
+	$username_bunda = $_POST['username_bunda'];
+	$password_bunda = $_POST['username_bunda'];
+	$passwordHash = password_hash($password_bunda, PASSWORD_DEFAULT);
+	$status_bunda = "Active";
+	$role = "Bunda";
+
+	// SET FOTO
+	$foto = $_FILES['foto_bunda']['name'];
+	$ext = pathinfo($foto, PATHINFO_EXTENSION);
+	$nama_foto = "image_".time().".".$ext;
+    $file_tmp = $_FILES['foto_bunda']['tmp_name'];
+
+    // TAMBAH DATA
+	$query= "INSERT INTO tb_bunda VALUES (NULL, '$nama_bunda', '$kontak_bunda', '$alamat_bunda', '$nama_foto', null, null)";
+	mysqli_query($conn, $query);
+	if (mysqli_affected_rows($conn) > 0) {
+		//get Id bunda
+		$getIdInster = mysqli_insert_id($conn);
+		// TAMBAH AKUN LOGIN bunda
+		$queryauth = "INSERT INTO tb_auth VALUES (NULL, '$getIdInster', '$username_bunda', '$passwordHash', '$status_bunda', '$role', null, null)";
+		mysqli_query($conn, $queryauth);
+        move_uploaded_file($file_tmp, '../../assets/dist/img/bunda/'.$nama_foto);
+		plugins(); ?>
+		<script>
+			$(document).ready(function() {
+				swal({
+					title: 'Berhasil',
+					text: 'Data Bunda Berhasil ditambah!',
+					icon: 'success'
+				}).then((data) => {
+					location.href = 'data.php';
+				});
+			});
+		</script>
+	<?php }
+}
+
+
+// UPDATE bunda
+if (isset($_POST['edit_bunda'])) {
+	$id_bunda = $_POST['id_bunda'];
+	$nama_bunda = $_POST['nama_bunda'];
+	$kontak_bunda = $_POST['kontak_bunda'];
+	$alamat_bunda = $_POST['alamat_bunda'];
+	$status_bunda = $_POST['status_bunda'];
+	$username_bunda = $_POST['username_bunda'];
+
+    // SET FOTO
+	if ($_FILES['foto_bunda']['name'] != '') {
+		$foto = $_FILES['foto_bunda']['name'];
+		$ext = pathinfo($foto, PATHINFO_EXTENSION);
+		$nama_foto = "image_".time().".".$ext;
+		$file_tmp = $_FILES['foto_bunda']['tmp_name'];
+		// HAPUS OLD FOTO
+		$target = "../../assets/dist/img/bunda/".$_POST['foto_now'];
+		if (file_exists($target) && $_POST['foto_now'] != 'foto_default.png') unlink($target);
+		// UPLOAD NEW FOTO
+		move_uploaded_file($file_tmp, '../../assets/dist/img/bunda/'.$nama_foto);
+	} else {
+		$nama_foto = $_POST['foto_now'];
+	}
+    $query = "UPDATE tb_bunda SET nama_bunda = '$nama_bunda',
+                                  kontak_bunda = '$kontak_bunda',
+                                  alamat_bunda = '$alamat_bunda',
+                                  foto_bunda = '$nama_foto',
+                                  update_at = null WHERE id_bunda = '$id_bunda'";
+		mysqli_query($conn, $query);
+	// EDIT PARTAI
+	if (mysqli_affected_rows($conn) > 0) {
+		$query2 = "UPDATE tb_auth SET username = '$username_bunda',
+									  status = '$status_bunda',
+									  update_at = null WHERE user_id = '$id_bunda' AND role = 'Bunda' ";
+		mysqli_query($conn, $query2);
+		plugins(); ?>
+		<script>
+			$(document).ready(function() {
+				swal({
+					title: 'Berhasil',
+					text: 'Data Bunda berhasil diubah',
+					icon: 'success'
+				}).then((data) => {
+					location.href = 'data.php';
+				});
+			});
+		</script>
+	<?php }
+}
+
+
+// HAPUS AREA
+if (isset($_GET['hapus_bunda'])) {
+	$id_bunda = $_GET['id_bunda'];
+
+	$query = "DELETE FROM tb_bunda WHERE id_bunda = '$id_bunda'";
+	$query2 = "DELETE FROM tb_auth WHERE user_id = '$id_bunda' AND role = 'Bunda' ";
+	mysqli_query($conn, $query);
+	mysqli_query($conn, $query2);
+	if (mysqli_affected_rows($conn) > 0) {
+		plugins(); ?>
+		<script>
+			$(document).ready(function() {
+				swal({
+					title: 'Berhasil Dihapus',
+					text: 'Data Bunda berhasil dihapus',
+					icon: 'success'
+				}).then((data) => {
+					location.href = 'data.php';
+				});
+			});
+		</script>
+	<?php }
+}
+
+
+
+?>
--- admin/bunda/data.php
+++ admin/bunda/data.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../template/header/header.php';
-$masyarakat = mysqli_query($conn, "SELECT * FROM tb_masyarakat");
+$bunda = mysqli_query($conn, "SELECT * FROM tb_bunda");
 ?>
 
   <!-- Content Wrapper. Contains page content -->
@@ -55,8 +55,68 @@ $masyarakat = mysqli_query($conn, "SELECT * FROM tb_masyarakat");
                   </div>
 
                 </div> -->
-                <a href="#" type="button" class="btn btn-primary"><i class="fa fa-plus-square"></i>&nbsp Tambah Bunda</a>
+                <button data-toggle="modal" data-target="#modal-lg" type="button" class="btn btn-primary"><i class="fa fa-plus-square"></i>&nbsp Tambah Bunda</a>
               </div>
+
+
+                <!-- Modal Tambah bunda -->
+                <div class="modal fade" id="modal-lg">
+                  <div class="modal-dialog modal-lg">
+                    <form method="POST" action="controller.php" enctype="multipart/form-data">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h4 class="modal-title">Tambah Bunda</h4>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+
+                        <div class="form-group">
+                          <label for="inputName">Nama Lengkap</label>
+                          <input type="text" id="nama_bunda" name="nama_bunda"class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                          <label for="inputName">Kontak</label>
+                          <input type="text" id="kontak_bunda" name="kontak_bunda"class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                          <label for="inputName">Alamat</label>
+                          <input type="text" id="alamat_bunda" name="alamat_bunda"class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                          <label for="inputName">Username</label>
+                          <input type="text" id="username_bunda" name="username_bunda"class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                          <label for="customFile">Foto</label>
+                          <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="foto_bunda" name="foto_bunda" onchange="readURL(this);" >
+                            <label class="custom-file-label" for="foto_bunda">Choose file</label>
+                          </div>
+                        </div>
+                        <br>
+                        <img style="max-width:180px; max-height:180px;" id="blah" src="/apmodasi/assets/dist/img/default-150x150.png" alt="your image" />
+
+
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                          <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                          <button type="submit" name="submit_bunda" class="btn btn-primary">Simpan</button>
+                        </div>
+                      </div>
+                    </form>
+                    <!-- /.modal-content -->
+                  </div>
+                  <!-- /.modal-dialog -->
+                </div>
+                <!-- /.modal -->
+
+
               <!-- /.card-header -->
               <div class="card-body">
                 <table id="example1" class="table table-bordered table-striped">
@@ -71,13 +131,23 @@ $masyarakat = mysqli_query($conn, "SELECT * FROM tb_masyarakat");
                   </tr>
                   </thead>
                   <tbody>
+                  <?php $i = 1; foreach($bunda as $dta) { ?>
                   <tr>
-                    <td style="text-align:center">1</td>
-                    <td>Foto Bunda</td>
-                    <td>Nama Bunda</td>
-                    <td>Kontak</td>
+                    <td style="text-align:center"><?= $i ?></td>
+                    <td style="text-align: center">
+                      <a href="/apmodasi/assets/dist/img/bunda//<?= $dta['foto_bunda'] ?>" data-toggle="lightbox" data-title="Nama : <?= $dta['nama_bunda'] ?>" data-gallery="gallery">
+                        <img src="/apmodasi/assets/dist/img/bunda//<?= $dta['foto_bunda'] ?>" border=3 height=60 width=60 class="img-fluid mb-2" alt="red sample"/>
+                      </a>
+                    </td>
+                    <td><?= $dta['nama_bunda'] ?></td>
+                    <td><?= $dta['kontak_bunda'] ?></td>
+                    <?php
+                      $bayi_bunda = mysqli_query($conn, "SELECT * FROM tb_bayi WHERE bunda_id = '$dta[id_bunda]' ");
+                      $row_bayi_bunda = mysqli_num_rows($bayi_bunda);
+                      $row_bayi_bunda_final = $row_bayi_bunda;
+                    ?>
                     <td style="text-align:center">
-                    <span class="badge badge-info">3 Orang</span>
+                    <span class="badge badge-info"><?= $row_bayi_bunda_final ?> Orang</span>
                     </td>
                     <td style="text-align:center; width: 20px;">
                       <div class="btn-group">
@@ -86,13 +156,123 @@ $masyarakat = mysqli_query($conn, "SELECT * FROM tb_masyarakat");
                         </button>
                         <div class="dropdown-menu dropdown-menu-right" role="menu">
                           <a href="#" class="dropdown-item">Detail</a>
-                          <a href="#" class="dropdown-item">Edit</a>
-                          <a href="#" data-toggle="modal" data-target="#modal-danger" class="dropdown-item">Hapus</a>
+                          <a href="#" data-toggle="modal" data-target="#modal-lg<?= $dta['id_bunda'] ?>"  class="dropdown-item">Edit</a>
+                          <a href="#" data-toggle="modal" data-target="#modal-danger<?= $dta['id_bunda'] ?>" class="dropdown-item">Hapus</a>
                         </div>
                       </div>
                     </td>
                   </tr>
 
+
+
+                <!-- Modal EDIT KADER -->
+                <div class="modal fade" id="modal-lg<?= $dta['id_bunda'] ?>">
+                  <div class="modal-dialog modal-lg">
+                    <form method="POST" action="controller.php" enctype="multipart/form-data">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h4 class="modal-title">Edit Bunda</h4>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+
+                        <div class="form-group">
+                          <label for="inputName">Nama Lengkap</label>
+                          <input type="text" value="<?= $dta['nama_bunda'] ?>" id="nama_bunda" name="nama_bunda"class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                          <label for="inputName">Kontak</label>
+                          <input type="text" value="<?= $dta['kontak_bunda'] ?>" id="kontak_bunda" name="kontak_bunda"class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                          <label for="inputName">Alamat</label>
+                          <input type="text" value="<?= $dta['alamat_bunda'] ?>" id="alamat_bunda" name="alamat_bunda"class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                          <label for="inputName">Username</label>
+                          <?php
+                            $get_auth = mysqli_query($conn, "SELECT * FROM tb_auth WHERE user_id = '$dta[id_bunda]' AND role = 'Bunda' ");
+                            $data_auth = mysqli_fetch_assoc($get_auth);
+                          ?>
+
+                          <input type="text" value="<?= $data_auth['username'] ?>" id="username_bunda" name="username_bunda"class="form-control">
+                        </div>
+
+                          <div class="form-group">
+                            <label for="inputName">Status Akun</label>
+                            <div class="col-sm-9">
+                              <select name="status_bunda" id="status_bunda" class="form-control">
+                              <?php
+                                if ($data_auth['status']=="Active"){
+                                  echo "<option selected='selected' value='Active'>Active</option>
+                                    <option value='Suspend'>Suspend</option>";
+                                } else{
+                                  echo "<option value='Active'>Active</option>
+                                    <option selected='selected' value='Suspend'>Suspend</option>";
+                                }
+                              ?>
+                              </select>
+                            </div>
+                          </div>
+
+                        <div class="form-group">
+                          <label for="customFile">Foto</label>
+                          <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="foto_bunda" name="foto_bunda" onchange="readURL(this);" >
+                            <label class="custom-file-label" for="foto_bunda">Choose file</label>
+                          </div>
+                        </div>
+                        <br>
+                        <img style="max-width:180px; max-height:180px;" id="blah" src="/apmodasi/assets/dist/img/bunda/<?= $dta['foto_bunda'] ?>" alt="your image" />
+
+
+
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                          <input type="hidden" name="id_bunda" value="<?= $dta['id_bunda'] ?>">
+                          <input type="hidden" name="foto_now" value="<?= $dta['foto_bunda'] ?>">
+                          <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                          <button type="submit" name="edit_bunda" class="btn btn-primary">Simpan</button>
+                        </div>
+                      </div>
+                    </form>
+                    <!-- /.modal-content -->
+                  </div>
+                  <!-- /.modal-dialog -->
+                </div>
+                <!-- /.modal -->
+
+      <!-- Modal Hapus -->
+      <div class="modal fade" id="modal-danger<?= $dta['id_bunda'] ?>">
+        <div class="modal-dialog">
+          <div class="modal-content bg-danger">
+            <div class="modal-header">
+              <h4 class="modal-title">Hapus Akun Bunda?</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>Yakin Ingin Menghapus Akun Bunda</p>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-outline-light" data-dismiss="modal">Batal</button>
+              <a href="controller.php?hapus_bunda=true&id_bunda=<?= $dta['id_bunda'] ?>" type="button" class="btn btn-outline-light">Hapus</a>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+      <!-- /.modal -->
+
+
+                  <?php $i = $i + 1; } ?>
                   </tbody>
 
                 </table>
