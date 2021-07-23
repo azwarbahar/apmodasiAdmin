7afe8e27a517c4ffb6da3a2f2fb1e9f2806bea81
azwarbahar07 <azwarbahar07@gmail.com>
2021-07-23 19:42:24 +0800

    fix bayi

--- api/bayi/editFotoBayi.php
+++ api/bayi/editFotoBayi.php
@@ -4,17 +4,17 @@
 
  $id_bayi = $_POST["id_bayi"];
  $foto_bayi = $_POST["foto_bayi"];
- $path = "/apmodasi/assets/dist/img/bayi/image_".time().".png";
+ $path = "../../assets/dist/img/bayi/image_".time().".png";
  $finalPath = "image_".time().".png";
 
- $query = "UPDATE tb_bayi SET foto_bayi = '$finalPath' WHERE id_bayi = '$id_bayi'";
+ $query = "UPDATE tb_bayi SET foto_bayi = '$finalPath',
+                                update_at = null WHERE id_bayi = '$id_bayi'";
 
  if (mysqli_query($conn, $query)) {
-     file_put_contents( $path, base64_decode($foto_bayi) );
+    file_put_contents( $path, base64_decode($foto_bayi) );
+    echo json_encode(array('kode' =>'1', 'pesan' => 'Berhasil Mengubah Data'));
 }
-
-//  $result = mysqli_query($conn, $query);
- $exeQuery =  mysqli_query($conn, $query);
- echo ($exeQuery) ? json_encode(array('kode' =>'1', 'pesan' => 'Berhasil Mengubah Data'
- )) : json_encode(array('kode' => '2', 'pesan' => 'Proses gagal'));
+else {
+   echo json_encode(array("kode" => "0", "pesan" => "Proses gagal"));
+}
 ?>
