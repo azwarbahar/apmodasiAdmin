c3bd8d6a5e974dbe4e0b95b12aae1525845750d3
azwarbahar07 <azwarbahar07@gmail.com>
2021-08-15 21:04:49 +0800

    fix add imunisai

--- admin/imunisasi/controller.php
+++ admin/imunisasi/controller.php
@@ -21,25 +21,27 @@ if (isset($_POST['submit_imunisasi'])) {
         $tanggal_imunisasi = $_POST['tanggal'.$no_imunisasi];
         $jenis_input = "Vaksin";
 
-        $query = "UPDATE tb_imunisasi SET usia_bayi_imunisasi = '$usia_bayi_imunisasi',
-                                            kader_id = '$kader_id',
-                                            status_imunisasi = '$status_imunisasi',
-                                            catatan_imunisasi = '$catatan_imunisasi',
-                                            tanggal_imunisasi = '$tanggal_imunisasi',
-                                            update_at = null WHERE bayi_id = '$id_bayi' AND no_imunisasi = '$no_imunisasi'";
-
-
-        $query2 = "INSERT INTO tb_riwayat_kader values(null,'$kader_id',
-                                                '$id_bayi',
-                                                '$usia_bayi_imunisasi',
-                                                '$no_imunisasi',
-                                                '$status_imunisasi',
-                                                '$catatan_imunisasi',
-                                                '$tanggal_imunisasi',
-                                                null,
-                                                null)";
-         mysqli_query($conn, $query);
-         mysqli_query($conn, $query2);
+        if (!$catatan_imunisasi == ""){
+            $query = "UPDATE tb_imunisasi SET usia_bayi_imunisasi = '$usia_bayi_imunisasi',
+                                                kader_id = '$kader_id',
+                                                status_imunisasi = '$status_imunisasi',
+                                                catatan_imunisasi = '$catatan_imunisasi',
+                                                tanggal_imunisasi = '$tanggal_imunisasi',
+                                                update_at = null WHERE bayi_id = '$id_bayi' AND no_imunisasi = '$no_imunisasi'";
+    
+    
+            $query2 = "INSERT INTO tb_riwayat_kader values(null,'$kader_id',
+                                                    '$id_bayi',
+                                                    '$usia_bayi_imunisasi',
+                                                    '$no_imunisasi',
+                                                    '$status_imunisasi',
+                                                    '$catatan_imunisasi',
+                                                    '$tanggal_imunisasi',
+                                                    null,
+                                                    null)";
+             mysqli_query($conn, $query);
+             mysqli_query($conn, $query2);
+        }
     }
 	if (mysqli_affected_rows($conn) > 0) {
 		plugins(); ?>
