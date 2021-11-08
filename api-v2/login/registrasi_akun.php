33925fb666aeed08378e95543a15968168397f20
azwarbahar07 <azwarbahar07@gmail.com>
2021-11-08 13:35:37 +0800

    cjhdfbgk

--- api-v2/login/registrasi_akun.php
+++ api-v2/login/registrasi_akun.php
@@ -2,24 +2,25 @@
 require_once '../../koneksi-v2.php';
 header('Content-type: application/json');
 
+$nik_bunda = $_POST['nik_bunda'];
 $nama_bunda = $_POST['nama_bunda'];
 $kontak_bunda = $_POST['kontak_bunda'];
 $alamat_bunda = $_POST['alamat_bunda'];
-$username_bunda = $_POST['username_bunda'];
+$kelurahan_bunda = $_POST['kelurahan_bunda'];
 $password_bunda = $_POST['password_bunda'];
 $passwordHash = password_hash($password_bunda, PASSWORD_DEFAULT);
 $status_bunda = "Inactive";
 $role = "Bunda";
 $nama_foto = "foto_default.png";
 // TAMBAH DATA
-$query= "INSERT INTO tb_bunda VALUES (NULL, '$nama_bunda', '$kontak_bunda', '$alamat_bunda', '$nama_foto', null, null)";
+$query= "INSERT INTO tb_bunda VALUES ('$nik_bunda', '$nama_bunda', '$kontak_bunda', '$alamat_bunda', '$kelurahan_bunda', '$nama_foto', null, null)";
 mysqli_query($conn, $query);
     if (mysqli_affected_rows($conn) > 0) {
 
         //get Id bunda
         $getIdInster = mysqli_insert_id($conn);
         // TAMBAH AKUN LOGIN bunda
-        $queryauth = "INSERT INTO tb_auth VALUES (NULL, '$getIdInster', '$username_bunda', '$passwordHash', '$status_bunda', '$role', null, null)";
+        $queryauth = "INSERT INTO tb_auth VALUES (NULL, '$nik_bunda', '$nik_bunda', '$passwordHash', '$status_bunda', '$role', null, null)";
         mysqli_query($conn, $queryauth);
         $result["kode"] = "1";
         $result["pesan"] = "Success";
